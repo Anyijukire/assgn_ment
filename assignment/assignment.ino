e03800d4bcc06aa7bbd38f2437e176a8d608682d
Anyijukire <79198971+Anyijukire@users.noreply.github.com>
2021-04-20 08:17:14 +0300

    Update assignment.ino

--- assignment/assignment.ino
+++ assignment/assignment.ino
@@ -2,9 +2,34 @@ int Red =1;
 int Blue=2;
 int Green=3;
 void setup(){
+  /*pinMode(Red, OUTPUT);
+  pinMode(Blue, OUTPUT);
+  pinMode(Green, OUTPUT);*/
   SETUP();
 }
 void loop(){
+  /*
+   int j=1;
+  while (j<=5)
+{ digitalWrite(RED, HIGH);
+    DELAY();
+  digitalWrite(RED, LOW);
+  DELAY();
+ j++; }
+  int j=1;
+  while (j<=5)
+{ digitalWrite(Blue, HIGH);
+    DELAY();
+  digitalWrite(Blue, LOW);
+  DELAY();
+ j++;}
+ for (int j=1; j<=5,j++;)
+{ digitalWrite(Green, HIGH);
+    DELAY();
+  digitalWrite(Green, LOW);
+  DELAY();
+ 
+}*/
   RED();
   BLUE();
   GREEN();
